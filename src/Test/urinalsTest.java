323c9edf8ea0f43bead61d2a28bd7da346fd5abc
Satvik Chemudupati <112653251+SatvikChemudupati@users.noreply.github.com>
2022-10-27 21:59:06 -0700

    3.0
    Updated the file read and write

--- src/Test/urinalsTest.java
+++ src/Test/urinalsTest.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
@@ -7,7 +10,14 @@ class urinalsTest {
         assertEquals(false, urinals.goodString("satv"));
     }
 
+
     @org.junit.jupiter.api.Test
-    void main() {
+    void countUrinalsTest() {
+        assertEquals( 1 , urinals.count_method("10001"));
+        assertEquals( 0 , urinals.count_method("1001"));
+        assertEquals( 3 , urinals.count_method("00000"));
+        assertEquals( 2 , urinals.count_method("0000"));
+        assertEquals( 1 , urinals.count_method("01000"));
     }
+
 }
--- src/src/urinals.java
+++ src/src/urinals.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * Implements a function that returns the maximum of free urinals as an integer
@@ -8,15 +10,164 @@
 public class urinals {
 
     public static boolean goodString( String str ) {
-        System.out.println("====== Satvik Chemudupati == TEST TWO EXECUTED =======");
-        if( str.matches("\"^[01]+$\""))
-            return true;
-        return false;
+            return (str.matches("[0-1]+"));
     }
 
+    static int count_method(String str1){
 
-    public static void main(String[] args){
+        int str_length = str1.length();
+        char[] chars = str1.toCharArray();
+        int count = 0;
+        int i;
+        for (i = 0; i < str_length - 1; i++){
+
+            if (chars[i] == '1') {
+                if (i >= 1) {
+                    if (chars[i + 1] == '1' || chars[i - 1] == '1') {
+                        return -1;
+                    }
+                }
+                else {
+                    if (chars[i + 1] == '1') {
+                        return -1;
+                    }
+                }
+            }
+            else if (chars [i] == '0')
+            {
+                if ( i == 0 && chars[i + 1] == '1')
+                {
+                    continue;
+                }
+                else if (i == 0){
+                    chars[i] = '1';
+                    count += 1;
+                }
+                if (i >= 1) {
+                    if (!(chars[i + 1] == '1' || chars[i - 1] == '1'))
+                    {
+                        chars[i] = '1';
+                        count += 1;
+                    }
+
+                }
+            }
+        }
+
+        if (i+1 == str_length && chars[i-1] == '1'){
+            return count;
+        }
+        else if (i+1 == str_length && chars[i-1] == '0'){
+            if (chars[i] != '1')
+            {
+                chars[i] = '1';
+                count += 1;
+            }
 
 
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        String str1;
+
+        System.out.println("Welcome!");
+        System.out.println("Please select the mode of input:");
+        System.out.println("1. Input text from keyboard \n2. Input text fom file \n");
+        int selection = sc.nextInt();
+        if (selection == 1)
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Please enter the string:");
+            str1 = reader.readLine();
+            boolean result = str1.matches("[0-1]+");
+            if (result)
+            {
+                int counter = count_method(str1);
+                System.out.println("The number of options are: " + counter);
+            }
+            else
+            {
+                System.out.println("Input contains values other than 0 and 1");
+            }
+        }
+        else if (selection == 2) {
+            File resourceFile = new File("input.dat");
+            if (resourceFile.exists() && !resourceFile.isDirectory()) {
+                System.out.println("Input File exists");
+                BufferedReader reader1 = new BufferedReader(new FileReader(resourceFile));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                String ls = System.getProperty("line.separator");
+                while ((line = reader1.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append(ls);
+                }
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+                reader1.close();
+                String sbString = stringBuilder.toString();
+                String[] ary = sbString.split("\n");
+                String[] counter = new String[ary.length];
+                for(int i = 0; i < ary.length; i++){
+                    str1 = ary[i];
+                    str1 = str1.substring(0, str1.length() - 1);
+                    if (goodString(str1)) {
+                        boolean result = str1.matches("[0-1]+");
+                        if (result) {
+                            counter[i] = String.valueOf(count_method(str1));
+                        } else {
+                            System.out.println("Input contains values other than 0 and 1");
+                        }
+                    }
+                    else {
+                        System.out.println("Failed good string");
+                    }
+                }
+                File f = new File("rule.txt");
+                int count_value = 1;
+                if (f.exists())
+                {
+                    System.out.println("File rule.txt exists. Checking next file");
+                    while (true) {
+                        File f1 = new File("rule" + count_value + ".txt");
+                        if (f1.exists() && !f1.isDirectory()) {
+                            System.out.println("File rule" + count_value + ".txt exists. Checking next file");
+                            count_value += 1;
+                        } else {
+                            System.out.println("File rule" + count_value + ".txt doesn't exist");
+                            break;
+                        }
+
+                    }
+                    System.out.println("Writing on " + "rule " + count_value + ".txt");
+                    FileWriter writer = new FileWriter("rule" + count_value + ".txt");
+                    for(String str2: counter) {
+                        writer.write(str2 + System.lineSeparator());
+                    }
+                    writer.close();
+                    System.out.println("Writing done..");
+                }
+                else
+                {
+                    FileWriter writer = new FileWriter("rule.txt");
+                    for(String str2: counter) {
+                        writer.write(str2 + System.lineSeparator());
+                    }
+                    writer.close();
+                    System.out.println("Writing done..");
+                }
+
+            } else {
+                System.out.println("File is not there");
+            }
+        }
+        else
+        {
+            System.out.println("-1");
+        }
     }
 }

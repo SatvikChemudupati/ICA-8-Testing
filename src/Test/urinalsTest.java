7ada918f9de21c2fca1e26b4d4ccc241f0004c23
Satvik Chemudupati <112653251+SatvikChemudupati@users.noreply.github.com>
2022-10-27 18:24:35 -0700

    Add files via upload

---
+++ src/Test/urinalsTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void goodString() {
+        assertEquals(false, urinals.goodString("satv"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
---
+++ src/src/urinals.java
@@ -0,0 +1,20 @@
+
+/**
+ * Implements a function that returns the maximum of free urinals as an integer
+ *
+ * @author  Satvik Chemudupati
+ */
+
+public class urinals {
+
+    public static boolean goodString( String str ) {
+        System.out.println ("====== Satvik Chemudupati == TEST ONE EXECUTED =======");
+        return true;
+    }
+
+
+    public static void main(String[] args){
+
+
+    }
+}
